f2eff323fa6249017e610e4f3ce4d951542c2068
JNamasaki <r.jmrsilva.j@gmail.com>
2023-01-16 09:29:05 -0300

    configurando server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
-import e from "express";
 
-const porta = e.env.PORT || 3000;
+
+const porta = process.env.PORT || 3000;
 
 app.listen(porta, () => {
     console.log(`Ouvindo na porta ${porta}`)
